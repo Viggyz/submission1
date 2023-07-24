8bf5bdf7136f04e282f742515417f3b0c7ec2d85
Vignesh Bhat <vignesh@kema.co>
2023-07-24 23:15:42 +0400

    Complete reply_factory

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -32,6 +32,16 @@ def record_current_answer(answer, current_question_id):
     '''
     Validates and stores the answer for the current question to session.
     '''
+    if isinstance(current_question_id, str): 
+        if not current_question_id.isnumeric():
+            return False, "Question id must be a number"
+        current_question_id = int(current_question_id)
+    if not isinstance(current_question_id, int):
+        return False, "Question id must be a positive integer"
+    if current_question_id < 0 or current_question_id > len(PYTHON_QUESTION_LIST):
+        return False, "Question id is invalid"
+    if PYTHON_QUESTION_LIST[current_question_id]["answer"] != answer:
+        return False, "Incorrect answer passed"
     return True, ""
 
 
@@ -39,8 +49,9 @@ def get_next_question(current_question_id):
     '''
     Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
     '''
-
-    return "dummy question", -1
+    if current_question_id!=len(PYTHON_QUESTION_LIST)-1:
+        return PYTHON_QUESTION_LIST[current_question_id+1], current_question_id+1
+    return None
 
 
 def generate_final_response(session):
@@ -48,5 +59,12 @@ def generate_final_response(session):
     Creates a final result message including a score based on the answers
     by the user for questions in the PYTHON_QUESTION_LIST.
     '''
-
-    return "dummy result"
+    matched_answers = set(map(lambda q_dict: q_dict["answer"], PYTHON_QUESTION_LIST)).intersection(
+        map(lambda message: message.text,
+            filter(
+                lambda message: message["is_user"],
+                session["message_history"]
+            )
+        )
+    )
+    return f"Thank you for playing, you score is {len(matched_answers)}"
